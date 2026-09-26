ebd9e429d3aacba86ee7d9cc2b3fbb77fbd3fd08
abdullahsaeed1211 <abdullah.saeed1724@gmail.com>
2026-09-26 21:56:42 +0530

    Replace 888 number with 917 across SEO and terms

--- resources/views/public/pages/terms.blade.php
+++ resources/views/public/pages/terms.blade.php
@@ -6,7 +6,7 @@
             <h1 class="text-4xl font-heading font-black uppercase text-asphaltBlack mb-6">{{ t('terms_of_service') }}</h1>
             <p class="text-gray-600 mb-4">{{ t('by_using_this_website_you_agree_to_these_terms_content_is_pr') }}</p>
             <p class="text-gray-600 mb-4">{{ t('enrollment_and_payments_are_subject_to_course_availability_a') }}</p>
-            <p class="text-gray-600">{{ t('for_support_contact_us_at_888_8722504_or_infoa1traininggroup') }}</p>
+            <p class="text-gray-600">{{ t('for_support_contact_us_at_917_7326520_or_infoa1traininggroup') }}</p>
         </div>
     </main>
 </x-layouts.public>
